62352953f6d4778d166938671d7edc67b33c82a1
Rebeca Belin <becabelin@gmail.com>
2021-11-05 08:54:42 -0300

    adicionando um form para adicionar pacientes

--- js/principal.js
+++ js/principal.js
@@ -4,6 +4,42 @@ titulo.textContent = "Aparecida Nutricionista"
 var paciente = document.querySelector("#primeiro-paciente");
 var pacientes = document.querySelectorAll(".paciente");
 
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener("click", function(event) {
+    event.preventDefault();
+
+    var form = document.querySelector("#form-adiciona");
+
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+
+    var pacienteTr = document.createElement("tr");
+
+    var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+
+    var tabela = document.querySelector("#tabela-pacientes");
+
+    tabela.appendChild(pacienteTr);
+
+
+});
+
 // calcular o imc
 function calcularImc(pacientes) {
     pacientes.forEach(function(paciente) {
