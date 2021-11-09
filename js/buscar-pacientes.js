c7feab9c827ff3bd5043449cda10f7d76518a3ac
Rebeca Belin <becabelin@gmail.com>
2021-11-09 15:55:56 -0300

    melhorando a usabilidade

--- js/buscar-pacientes.js
+++ js/buscar-pacientes.js
@@ -1,20 +1,42 @@
 var botaoAdicionar = document.querySelector("#buscar-pacientes");
 
 botaoAdicionar.addEventListener("click", function() {
-    console.log("Buscando pacientes...");
+    var xhr = new XMLHttpRequest();
+
+    xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
+
+    xhr.addEventListener("load", function() {
+        var erroAjax = document.querySelector("#erro-ajax");
+
+        if (xhr.status == 200) {
+            var resposta = xhr.responseText;
+            var pacientes = JSON.parse(resposta);
+
+            pacientes.forEach(function(paciente) {
+                adicionaPacienteNaTabela(paciente);
+            });
+        } else {
+            erroAjax.classList.remove("invisivel");
+        }
+    });
 
     var xhr = new XMLHttpRequest();
 
     xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
 
     xhr.addEventListener("load", function() {
-        var resposta = xhr.responseText;
+        var erroAjax = document.querySelector("#erro-ajax");
+        if (xhr.status == 200) {
+            erroAjax.classList.add("invisivel");
+            var resposta = xhr.responseText;
+            var pacientes = JSON.parse(resposta);
 
-        var pacientes = JSON.parse(resposta);
-
-        pacientes.forEach(function(paciente) {
-            adicionaPacienteNaTabela(paciente);
-        });
+            pacientes.forEach(function(paciente) {
+                adicionaPacienteNaTabela(paciente);
+            });
+        } else {
+            erroAjax.classList.remove("invisivel");
+        }
     });
 
     xhr.send();
